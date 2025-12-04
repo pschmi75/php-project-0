88aebf85e8b64a4b08eaeb96872074f3cc860d4c
Philipp Schmitz <philipp.schmitz3@alumni.fh-aachen.de>
2025-12-04 11:48:21 +0100

    password gen finished, only works with php 8.3+

---
+++ pwgen/password-gen.php
@@ -0,0 +1,41 @@
+<?php
+  if ($_POST["length"] !== NULL) {
+  $pwlen = (int) $_POST["length"];
+  $rand = new \Random\Randomizer();
+  $srcstr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!&/()=?#+*";
+  
+  switch (true) {
+    case $pwlen < 1:
+      $pw = "Length must be greater than 0"
+      break;
+    default:
+      $pw = $rand->getBytesFromString($srcstr, $pwlen);      
+      break;
+  }
+
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Random Password Generator</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="title-container">
+    <h1>Password Generator </h1>
+    <h3>Your Password:</h3>
+  </div>
+  <div class="password-container">
+    <h5><?php echo $pw ? $pw : "" ?></h5>
+  </div>
+  <form method="POST" action="#">
+    <p>How long should your password be?</p>
+    <input class="number-input" type="number" name="length" value="8" min="1" required>
+    <input class="submit-input" type="submit" value="generate">
+  </form>
+</body>
+</html>
